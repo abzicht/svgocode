9381ce631c23718749916cf0e4baceff06e4c8fb
Moritz Finke <abzicht@gmail.com>
2025-11-02 16:48:55 +0100

    Added COPYING

--- svgocode/flags.go
+++ svgocode/flags.go
@@ -8,15 +8,17 @@ import (
 type Flags struct {
 	Verbosity             int    `short:"v" long:"verbosity" description:"Verbosity (fatal: 0, error: 1, warn: 2, info: 3, debug: 4)." default:"3"`
 	SvgFile               string `short:"s" long:"svg" description:"SVG file to read from (in place of STDIN)"`
-	GcodeFile             string `short:"g" long:"gcode" description:"File that will gcode will be written to (in place of STDOUT)"`
+	GcodeFile             string `short:"g" long:"gcode" description:"File that GCODE will be written to (in place of STDOUT)"`
 	PlotterConfigFile     string `short:"p" long:"plotter-config" description:"YAML-encoded config file for the plotter that is to be used."`
 	PlotterConfigTemplate bool   `long:"plotter-config-template" description:"Print an exemplary plotter configuration file in YAML-encoding (cf. flag --plotter-config)"`
-	Ordering              string `short:"o" long:"ordering-algoritm" description:"Algorithm for finding a gcode segment order. Available algorithms: '2opt' (perfect result, for small input), 'greedy' (not perfect, for large input), and 'none' (skip ordering)." default:"2opt"`
+	Ordering              string `short:"o" long:"ordering-algoritm" description:"Algorithm for finding a GCODE segment order. Available algorithms: '2opt' (perfect result; for small input), 'greedy' (not perfect; for large input), and 'none' (skip ordering)." default:"2opt"`
 }
 
 func ParseFlags(f *Flags) error {
 	parser := flags.NewParser(f, flags.Default)
-	var description string = "SVGOCODE is a(nother) tool for converting SVG files to Gcode"
+	var description string = `SVGOCODE: A(nother) tool for converting SVG files to GCODE.
+Copyright (C) 2025 Abzicht <https://github.com/abzicht>.
+This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under the GNU GPLv3 license (see <https://www.gnu.org/licenses/>).`
 
 	parser.LongDescription = description
 	_, err := parser.Parse()
